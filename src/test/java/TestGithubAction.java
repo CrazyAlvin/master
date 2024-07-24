c907b56755932c7ccba595f4061ea058c630f3cb
CrazyAlvin <alvinalvares.it@gmail.com>
2024-07-24 13:34:46 -0400

    create tests

---
+++ src/test/java/TestGithubAction.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestGithubAction {
+
+
+    @Test
+    public void test01(){
+        Assertions.assertEquals(5,5);
+    }
+
+
+
+    @Test
+    public void test02(){
+        Assertions.assertEquals(5,5);
+    }
+
+
+    @Test
+    public void test03(){
+        Assertions.assertEquals(5,5);
+    }
+
+}
